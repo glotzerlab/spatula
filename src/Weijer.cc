140969f6e809f1eb9effae8780365b5ebd7a3870
Brandon Butler <butlerbr@umich.edu>
2022-09-08 10:21:46 -0400

    squash: Documentation additions
    Also makes symmetrize_qlm a helper function for symmetrize_qlms.

--- src/Weijer.cc
+++ src/Weijer.cc
@@ -3,29 +3,33 @@
 #include "Weijer.h"
 
 void symmetrize_qlms(const std::vector<std::complex<double>>& qlms,
-                     const std::vector<std::vector<std::complex<double>>>& D_ij,
+                     const std::vector<std::vector<std::complex<double>>>& D_ijs,
                      std::vector<std::vector<std::complex<double>>>& sym_qlm_buf,
                      unsigned int max_l)
 {
-    size_t num_syms = D_ij.size();
+    for (size_t sym_i {0}; sym_i < D_ijs.size(); ++sym_i) {
+        symmetrize_qlm(qlms, D_ijs[sym_i], sym_qlm_buf[sym_i], max_l);
+    }
+}
 
-    for (size_t sym_i {0}; sym_i < num_syms; ++sym_i) {
-        auto& sym_i_qlms = sym_qlm_buf[sym_i];
-        sym_i_qlms.clear();
-        const auto& d_ij = D_ij[sym_i];
-        size_t qlm_i {0};
-        size_t dij_index {0};
-        for (size_t l {0}; l < max_l + 1; ++l) {
-            size_t max_m {2 * l + 1};
-            for (size_t m_prime {0}; m_prime < max_m; ++m_prime) {
-                std::complex<double> sym_qlm {0.0, 0.0};
-                for (size_t m {0}; m < max_m; ++m) {
-                    sym_qlm += qlms[qlm_i + m] * d_ij[dij_index];
-                    ++dij_index;
-                }
-                sym_i_qlms.emplace_back(sym_qlm);
+void symmetrize_qlm(const std::vector<std::complex<double>>& qlms,
+                    const std::vector<std::complex<double>>& D_ij,
+                    std::vector<std::complex<double>>& sym_qlm_buf,
+                    unsigned int max_l)
+{
+    sym_qlm_buf.clear();
+    size_t qlm_i {0};
+    size_t dij_index {0};
+    for (size_t l {0}; l < max_l + 1; ++l) {
+        const size_t max_m {2 * l + 1};
+        for (size_t m_prime {0}; m_prime < max_m; ++m_prime) {
+            std::complex<double> sym_qlm {0.0, 0.0};
+            for (size_t m {0}; m < max_m; ++m) {
+                sym_qlm += qlms[qlm_i + m] * D_ij[dij_index];
+                ++dij_index;
             }
-            qlm_i += max_m;
+            sym_qlm_buf.emplace_back(sym_qlm);
         }
+        qlm_i += max_m;
     }
 }
