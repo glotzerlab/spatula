6f9434322e497e1fa23c881f82141098ed0d4018
Brandon Butler <butlerbr@umich.edu>
2022-09-12 11:23:59 -0400

    refactor: Move Vec3 to its own file.

---
+++ src/Vec3.cc
@@ -0,0 +1,125 @@
+#include "Vec3.h"
+
+Vec3::Vec3(double x_, double y_, double z_) : x(x_), y(y_), z(z_) { }
+
+Vec3::Vec3(const double* point) : x(point[0]), y(point[1]), z(point[2]) { }
+
+Vec3::Vec3() : x(0.0), y(0.0), z(0.0) { }
+
+double Vec3::dot(const Vec3& b) const
+{
+    return x * b.x + y * b.y + z * b.z;
+}
+
+template<typename number_type> Vec3 operator+(const Vec3& a, const number_type& b)
+{
+    return Vec3(a.x + b, a.y + b, a.z + b);
+}
+
+template<typename number_type> Vec3 operator-(const Vec3& a, const number_type& b)
+{
+    return Vec3(a.x - b, a.y - b, a.z - b);
+}
+
+template<typename number_type> Vec3 operator*(const Vec3& a, const number_type& b)
+{
+    return Vec3(a.x * b, a.y * b, a.z * b);
+}
+
+template<typename number_type> Vec3 operator/(const Vec3& a, const number_type& b)
+{
+    return Vec3(a.x / b, a.y / b, a.z / b);
+}
+
+template<> Vec3 operator+(const Vec3& a, const Vec3& b)
+{
+    return Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+template<> Vec3 operator-(const Vec3& a, const Vec3& b)
+{
+    return Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+template<> Vec3 operator*(const Vec3& a, const Vec3& b)
+{
+    return Vec3(a.x * b.x, a.y * b.y, a.z * b.z);
+}
+
+template<> Vec3 operator/(const Vec3& a, const Vec3& b)
+{
+    return Vec3(a.x / b.x, a.y / b.y, a.z / b.z);
+}
+
+template<typename number_type> Vec3& operator+=(Vec3& a, const number_type& b)
+{
+    a.x += b;
+    a.y += b;
+    a.z += b;
+    return a;
+}
+
+template<typename number_type> Vec3& operator-=(Vec3& a, const number_type& b)
+{
+    a.x -= b;
+    a.y -= b;
+    a.z -= b;
+    return a;
+}
+
+template<typename number_type> Vec3& operator*=(Vec3& a, const number_type& b)
+{
+    a.x *= b;
+    a.y *= b;
+    a.z *= b;
+    return a;
+}
+
+template<typename number_type> Vec3& operator/=(Vec3& a, const number_type& b)
+{
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+    return a;
+}
+
+template<> Vec3& operator+=(Vec3& a, const Vec3& b)
+{
+    a.x += b.x;
+    a.y += b.y;
+    a.z += b.z;
+    return a;
+}
+
+template<> Vec3& operator-=(Vec3& a, const Vec3& b)
+{
+    a.x -= b.x;
+    a.y -= b.y;
+    a.z -= b.z;
+    return a;
+}
+
+template<> Vec3& operator*=(Vec3& a, const Vec3& b)
+{
+    a.x *= b.x;
+    a.y *= b.y;
+    a.z *= b.z;
+    return a;
+}
+
+template<> Vec3& operator/=(Vec3& a, const Vec3& b)
+{
+    a.x /= b.x;
+    a.y /= b.y;
+    a.z /= b.z;
+    return a;
+}
+
+template Vec3 operator+(const Vec3& a, const double& b);
+template Vec3 operator-(const Vec3& a, const double& b);
+template Vec3 operator*(const Vec3& a, const double& b);
+template Vec3 operator/(const Vec3& a, const double& b);
+template Vec3& operator+=(Vec3& a, const double& b);
+template Vec3& operator-=(Vec3& a, const double& b);
+template Vec3& operator*=(Vec3& a, const double& b);
+template Vec3& operator/=(Vec3& a, const double& b);
