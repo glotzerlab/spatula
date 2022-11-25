7496d3192791dfd38a32a92aade80c8374c2a71e
Brandon Butler <butlerbr@umich.edu>
2022-11-25 15:39:10 -0500

    feat: Provide a Mesh classmethod for equadistance rotations

--- pgop/optimize.py
+++ pgop/optimize.py
@@ -1,4 +1,6 @@
 import numpy as np
+import rowan
+import scipy as sp
 
 from . import _pgop
 
@@ -14,8 +16,71 @@ class LocalSearch:
 
 
 class Mesh:
+    _dodehedron_vertices = np.array(
+        [
+            [-0.69813, 0.0, 0.13333],
+            [0.69813, 0.0, -0.13333],
+            [-0.21573, -0.66396, 0.13333],
+            [-0.21573, 0.66396, 0.13333],
+            [0.5648, -0.41035, 0.13333],
+            [0.5648, 0.41035, 0.13333],
+            [-0.13333, -0.41035, 0.5648],
+            [-0.13333, 0.41035, 0.5648],
+            [-0.34907, -0.25361, -0.5648],
+            [-0.34907, 0.25361, -0.5648],
+            [0.34907, -0.25361, 0.5648],
+            [0.34907, 0.25361, 0.5648],
+            [0.43147, 0.0, -0.5648],
+            [-0.5648, -0.41035, -0.13333],
+            [-0.5648, 0.41035, -0.13333],
+            [-0.43147, 0.0, 0.5648],
+            [0.13333, -0.41035, -0.5648],
+            [0.13333, 0.41035, -0.5648],
+            [0.21573, -0.66396, -0.13333],
+            [0.21573, 0.66396, -0.13333],
+        ]
+    )
+
     def __init__(self, points):
         self._cpp = _pgop.Mesh([_pgop.Quaternion(p) for p in points])
+
+    @classmethod
+    def from_grid(cls, n_angles=6):
+        points = np.empty(
+            (n_angles * len(cls._dodehedron_vertices) + 1, 4), dtype=float
+        )
+        points[0] = np.array([1.0, 0.0, 0.0, 0.0])
+        angles = cls.sample_angles(n_angles)
+        points[1:] = rowan.from_axis_angle(
+            cls._dodehedron_vertices[:, None], angles[None, :]
+        ).reshape((-1, 4))
+        return cls(points)
+
+    @staticmethod
+    def sample_angles(n_angles):
+        def cdf(theta, root):
+            ipi = 1 / np.pi
+            sinx = np.sin(theta)
+            return (
+                ipi * (theta - sinx) - root,
+                ipi * (1 - np.cos(theta)),
+                ipi * sinx,
+            )
+
+        roots = np.linspace(0, 1, n_angles + 1)[1:]
+        angles = []
+        for r in roots:
+            opt = sp.optimize.root_scalar(
+                cdf,
+                args=(r,),
+                method="halley",
+                bracket=[0, np.pi],
+                x0=r * np.pi,
+                fprime=True,
+                fprime2=True,
+            )
+            angles.append(opt.root)
+        return np.array(angles)
 
 
 class LocalMonteCarlo:
