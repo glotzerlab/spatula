deca00fefb62dc35c28ef1a89932d4f7eb2d6b7e
Brandon Butler <butlerbr@umich.edu>
2024-02-13 16:43:44 -0500

    doc: Further util documentation additions/improvements

--- src/util/Threads.h
+++ src/util/Threads.h
@@ -8,37 +8,56 @@
 namespace py = pybind11;
 
 namespace pgop { namespace util {
+/**
+ * @brief Helper class for handle the parallelization logic for the pgop module.
+ *
+ * ThreadPool is a singleton which stores the state of parallelization and thread pools for the
+ * entire application.
+ *
+ * The singleton defaults to using every available thread.
+ */
 class ThreadPool {
     public:
+    /// Get the ThreadPool singletom
     static ThreadPool& get()
     {
         static ThreadPool threads;
         return threads;
     }
 
+    /// Get the current thread pool
     BS::thread_pool& get_pool()
     {
         return m_pool;
     }
 
+    /// Get the synced std::out like stream for output
     BS::synced_stream& get_synced_out()
     {
         return m_out;
     }
 
+    /// Set the number of threads to run PGOP on.
     void set_threads(unsigned int num_threads)
     {
         m_pool.reset(num_threads);
     }
 
+    /// Get the current number of threads in the thread pool.
     size_t get_num_threads()
     {
         return m_pool.get_thread_count();
     }
     /**
-     * @brief enable the serial execution of a given loop. This exists to help with profiling, as
-     * this enables profilers like py-spy to determine the slow elements of the loop for
-     * optimization purposes.
+     * @brief enable the serial execution of a given loop.
+     *
+     * This exists to help with profiling, as this enables profilers like py-spy to determine the
+     * slow elements of the loop for optimization purposes.
+     *
+     * @param start the first index of the loop
+     * @param end the last exclusive index of the loop
+     * @param loop A function that takes the two indices start and stop and optionally returns
+     * something.
      */
     template<typename return_type, typename index_type>
     return_type serial_compute(index_type start,
@@ -53,7 +72,7 @@ class ThreadPool {
 
     private:
     ThreadPool() : m_out(), m_pool() { }
-    // Must be before m_pool to ensure construction before the thread pull to
+    // Must be before m_pool to ensure construction before the thread pool to
     // avoid crashes.
     BS::synced_stream m_out;
     BS::thread_pool m_pool;
