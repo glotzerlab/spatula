382dee9bf76caaa57e12f260a85a15b8754c44ed
Brandon Butler <butlerbr@umich.edu>
2024-02-15 15:53:47 -0500

    refactor: Import util into main package

--- pgop/__init__.py
+++ pgop/__init__.py
@@ -1,4 +1,4 @@
-from . import bond_order, integrate, optimize, sph_harm
+from . import bond_order, integrate, optimize, sph_harm, util
 from .pgop import PGOP
 
-__all__ = ["bond_order", "integrate", "optimize", "sph_harm", "PGOP"]
+__all__ = ["bond_order", "integrate", "optimize", "sph_harm", "PGOP", "util"]
