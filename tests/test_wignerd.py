1b527fee2e7cdbe7da7dbbd868c1fff8acb6eec3
Domagoj Fijan <domagoj.fijan@gmail.com>
2024-06-14 18:46:09 -0400

    test against old matrices

--- tests/test_wignerd.py
+++ tests/test_wignerd.py
@@ -1,3 +1,4 @@
+import h5py
 import numpy as np
 import pytest
 
@@ -111,20 +112,38 @@ def test_D2_from_operations():
 
 def test_D2_semidirect_product():
     # C2'={E, 2_y}
-    WignerD_C2prime = condensed_wignerD_from_operations([identity(maxl), two_y(maxl)])
+    wignerd_c2prime = condensed_wignerD_from_operations([identity(maxl), two_y(maxl)])
     # D2=C2 x| C2'
     assert np.isclose(
-        semidirect_product(Cn(maxl, 2), WignerD_C2prime), Dn(maxl, 2)
+        semidirect_product(Cn(maxl, 2), wignerd_c2prime), Dn(maxl, 2)
     ).all()
 
 
 def test_D2_direct_product():
     # C2'={E, 2_y}
-    WignerD_C2prime = condensed_wignerD_from_operations([identity(maxl), two_y(maxl)])
-    C2prime = WignerD.from_condensed_matrix_maxl_point_group(
-        WignerD_C2prime, maxl, "C2'"
+    wignerd_c2prime = condensed_wignerD_from_operations([identity(maxl), two_y(maxl)])
+    c2prime = WignerD.from_condensed_matrix_maxl_point_group(
+        wignerd_c2prime, maxl, "C2'"
     )
     # D2=C2 x C2'
     assert np.isclose(
-        direct_product(WignerD("C2", maxl).matrices, C2prime.matrices), Dn(maxl, 2)
+        direct_product(WignerD("C2", maxl).matrices, c2prime.matrices), Dn(maxl, 2)
     ).all()
+
+
+def test_against_old_data():
+    with h5py.File("tests/data/data.h5", "r") as file:
+        point_groups = file["data"]["matrices"].attrs["point_groups"]
+        matrices = file["data"]["matrices"]
+        for point_group, matrix in zip(point_groups, matrices):
+            # TODO remove when TOI gets implemented
+            if point_group in "TOI":
+                continue
+            if point_group == "Ci":
+                assert np.isclose(
+                    WignerD(point_group, 12).condensed_matrices / 2, matrix
+                ).all()
+            else:
+                assert np.isclose(
+                    WignerD(point_group, 12).condensed_matrices, matrix
+                ).all()
