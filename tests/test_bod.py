4b12a55c897baca1f182d756c74695e3e29bd535
janbridley <bradleyjenj@gmail.com>
2024-06-27 12:10:35 -0400

    Fix tests

--- tests/test_bod.py
+++ tests/test_bod.py
@@ -7,5 +7,10 @@ import pgop.bond_order
 @pytest.mark.parametrize(
     "module", [m for m in dir(pgop.bond_order) if m[:4] == "Bond"]
 )
-def test_imports(module):
-    __import__("pgop." + module)
+def test_constructor(module):
+    bond_order = getattr(pgop.bond_order, module)
+    distribution = module[9:]
+
+    positions = [[0, 1, 0]]
+
+    bond_order() if distribution == "" else bond_order(positions, 0.1)
