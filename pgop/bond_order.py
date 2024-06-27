61338899427ef4db5485f4c338dc385a3974f3c7
janbridley <bradleyjenj@gmail.com>
2024-06-27 12:10:48 -0400

    Fix BOD python layer

--- pgop/bond_order.py
+++ pgop/bond_order.py
@@ -1,5 +1,10 @@
 """Simply bond order diagram classes."""
 
+# TODO: We should have a constructor that takes a distribution, and allows for
+# later calls to pass in the positions and weights.
+
+import numpy as np
+
 import pgop._pgop
 
 
@@ -24,7 +29,7 @@ class BondOrder:
 class BondOrderFisher(BondOrder):
     """BOD where neighbors are marked by von-Mises Fisher distributions."""
 
-    def __init__(self, positions, kappa):
+    def __init__(self, positions, kappa, weights=1.0):
         """Create a BondOrderFisher object.
 
         Parameters
@@ -33,16 +38,26 @@ class BondOrderFisher(BondOrder):
             The neighbor positions for the BOD.
         kappa : float
             The concentration parameter for the von-Mises Fisher distribution.
+        weights : float or np.ndarray[float]
+            Weights for each point. If a scalar is passed, it is expanded to
+            len(positions). Default value = 1.
         """
-        self._cpp = pgop._pgop.FisherBondOrder(
-            pgop._pgop.FisherDistribution(kappa), positions
+        if not hasattr(weights, "__len__"):
+            weights = np.full(shape=len(positions), fill_value=weights)
+
+        self._cpp = pgop._pgop.BondOrderFisher(
+            pgop._pgop.FisherDistribution(kappa),
+            # TODO: this is dumb.
+            # Correct code should be np.asarray(positions,dtype=np.float64)
+            [pgop._pgop.Vec3(*row) for row in positions],
+            weights,
         )
 
 
 class BondOrderUniform(BondOrder):
     """BOD where neighbors are marked by uniform distributions."""
 
-    def __init__(self, positions, max_theta):
+    def __init__(self, positions, max_theta, weights=1.0):
         """Create a BondOrderUniform object.
 
         Parameters
@@ -52,7 +67,15 @@ class BondOrderUniform(BondOrder):
         max_theta : float
             The distance from the distribution center where the density is
             non-zero for the uniform distribution.
+        weights : float or np.ndarray[float]
+            Weights for each point. If a scalar is passed, it is expanded to
+            len(positions). Default value = 1.
         """
-        self._cpp = pgop._pgop.UniformBondOrder(
-            pgop._pgop.UniformDistribution(max_theta), positions
+        if not hasattr(weights, "__len__"):
+            weights = np.full(shape=len(positions), fill_value=weights)
+
+        self._cpp = pgop._pgop.BondOrderUniform(
+            pgop._pgop.UniformDistribution(max_theta),
+            [pgop._pgop.Vec3(*row) for row in positions],
+            weights,
         )
