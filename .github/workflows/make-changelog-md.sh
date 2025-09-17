676818037a9adbdd7425f003e3ebc0ba341463fe
Domagoj Fijan <domagoj.fijan@gmail.com>
2025-09-17 16:28:41 -0400

    fix the changelog script

--- .github/workflows/make-changelog-md.sh
+++ .github/workflows/make-changelog-md.sh
@@ -4,17 +4,11 @@ set -euo pipefail
 
 rst_in="changelog.rst"
 
-# Fallback: output everything after the first '^^^^' underline
-fallback() {
-  awk '
-    found { print; next }
-    /^[[:space:]]*\^{4,}[[:space:]]*$/ { found=1; next }
-  ' "'"$rst_in"'" | pandoc --from=rst --to=markdown --wrap=none
-}
-
 # No tag provided -> fallback
 if [[ -z "${1:-}" ]]; then
-  fallback
+  pcregrep -M "^\^{4,}.*\n[\s\S]*" ${rst_in} \
+  | tail -n +2 \
+  | pandoc --from=rst --to=markdown --wrap=none  
   exit 0
 fi
 
