9854891334aea443dc3b8c13ae4fad739a6c623d
janbridley <bradleyjenj@gmail.com>
2025-11-17 19:04:54 -0500

    Threads header only

--- src/util/Threads.cc
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2021-2025 The Regents of the University of Michigan
-// Part of spatula, released under the BSD 3-Clause License.
-
-#include "Threads.h"
-
-namespace spatula { namespace util {
-}} // End namespace spatula::util
