2aa9e12fbe97da93e853ccfd9544395e29c18d33
janbridley <bradleyjenj@gmail.com>
2025-11-17 21:46:03 -0500

    Export data

--- src/data/Vec3.h
+++ src/data/Vec3.h
@@ -224,4 +224,13 @@ inline bool operator==(const Vec3& a, const Vec3& b)
 {
     return a.x == b.x && a.y == b.y && a.z == b.z;
 }
+
+template Vec3 operator+(const Vec3& a, const double& b);
+template Vec3 operator-(const Vec3& a, const double& b);
+template Vec3 operator*(const Vec3& a, const double& b);
+template Vec3 operator/(const Vec3& a, const double& b);
+template Vec3& operator+=(Vec3& a, const double& b);
+template Vec3& operator-=(Vec3& a, const double& b);
+template Vec3& operator*=(Vec3& a, const double& b);
+template Vec3& operator/=(Vec3& a, const double& b);
 }} // namespace spatula::data
