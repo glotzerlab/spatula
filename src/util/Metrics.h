1600939c05adbaedad04edc6c77995ccec624b25
janbridley <bradleyjenj@gmail.com>
2025-11-17 19:06:05 -0500

    Metrics -> header

--- src/util/Metrics.h
+++ src/util/Metrics.h
@@ -5,6 +5,7 @@
 
 #include <complex>
 #include <vector>
+#include <numeric> // Added for std::sqrt
 
 namespace spatula { namespace util {
 /**
@@ -17,6 +18,22 @@ namespace spatula { namespace util {
  * @param g The coefficents for the second spherical harmonic expansion
  * @returns A vector of the Pearson correlation for the two expansions
  */
-double covariance(const std::vector<std::complex<double>>& f,
-                  const std::vector<std::complex<double>>& g);
+inline double covariance(const std::vector<std::complex<double>>& f,
+                         const std::vector<std::complex<double>>& g)
+{
+    // For the covariance we must skip the first element as it adds a spurious
+    // detection of symmetry/covariance.
+    double f_cov = 0;
+    double g_covar = 0;
+    double mixed_covar = 0;
+    for (size_t j {1}; j < f.size(); ++j) {
+        f_cov += std::norm(f[j]);
+        g_covar += std::norm(g[j]);
+        mixed_covar += std::real(f[j] * std::conj(g[j]));
+    }
+    if (f_cov == 0 || g_covar == 0) {
+        return 0;
+    }
+    return mixed_covar / std::sqrt(g_covar * f_cov);
+}
 }} // namespace spatula::util
