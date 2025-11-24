8d888a30a532793555dec47648df2b306f37e23a
janbridley <bradleyjenj@gmail.com>
2025-11-24 12:16:57 -0500

    Random samples

--- src/benchmarks/Metrics.cpp
+++ src/benchmarks/Metrics.cpp
@@ -9,14 +9,34 @@
 // Benchmark for spatula::util::compute_Bhattacharyya_coefficient_gaussian
 static void BM_Bhattacharyya_Gaussian(benchmark::State& state)
 {
-    spatula::data::Vec3 pos1(1.0, 2.0, 3.0);
-    spatula::data::Vec3 pos2(1.1, 2.2, 3.3);
-    double sigma1 = 0.5;
-    double sigma2 = 0.6;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> distrib(-5.0, 5.0);
+    std::uniform_real_distribution<double> sigma_distrib(0.1, 1.0);
 
+    struct Sample {
+        spatula::data::Vec3 pos1;
+        spatula::data::Vec3 pos2;
+        double sigma1;
+        double sigma2;
+    };
+
+    std::vector<Sample> samples(1024);
+    for (auto& s : samples) {
+        s.pos1 = {distrib(gen), distrib(gen), distrib(gen)};
+        s.pos2 = {distrib(gen), distrib(gen), distrib(gen)};
+        s.sigma1 = sigma_distrib(gen);
+        s.sigma2 = sigma_distrib(gen);
+    }
+
+    size_t i = 0;
     for (auto _ : state) {
+        const auto& s = samples[i++ % samples.size()];
         benchmark::DoNotOptimize(
-            spatula::util::compute_Bhattacharyya_coefficient_gaussian(pos1, pos2, sigma1, sigma2));
+            spatula::util::compute_Bhattacharyya_coefficient_gaussian(s.pos1,
+                                                                      s.pos2,
+                                                                      s.sigma1,
+                                                                      s.sigma2));
     }
 }
 BENCHMARK(BM_Bhattacharyya_Gaussian);
@@ -24,14 +44,35 @@ BENCHMARK(BM_Bhattacharyya_Gaussian);
 // Benchmark for spatula::util::compute_Bhattacharyya_coefficient_fisher
 static void BM_Bhattacharyya_Fisher(benchmark::State& state)
 {
-    spatula::data::Vec3 pos1(1.0, 0.0, 0.0);
-    spatula::data::Vec3 pos2(0.9, 0.1, 0.0);
-    double kappa1 = 10.0;
-    double kappa2 = 12.0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> distrib(-1.0, 1.0);
+    std::uniform_real_distribution<double> kappa_distrib(1.0, 20.0);
 
+    struct Sample {
+        spatula::data::Vec3 pos1;
+        spatula::data::Vec3 pos2;
+        double kappa1;
+        double kappa2;
+    };
+
+    std::vector<Sample> samples(1024);
+    for (auto& s : samples) {
+        s.pos1 = {distrib(gen), distrib(gen), distrib(gen)};
+        s.pos1.normalize();
+        s.pos2 = {distrib(gen), distrib(gen), distrib(gen)};
+        s.pos2.normalize();
+        s.kappa1 = kappa_distrib(gen);
+        s.kappa2 = kappa_distrib(gen);
+    }
+
+    size_t i = 0;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(
-            spatula::util::compute_Bhattacharyya_coefficient_fisher(pos1, pos2, kappa1, kappa2));
+        const auto& s = samples[i++ % samples.size()];
+        benchmark::DoNotOptimize(spatula::util::compute_Bhattacharyya_coefficient_fisher(s.pos1,
+                                                                                         s.pos2,
+                                                                                         s.kappa1,
+                                                                                         s.kappa2));
     }
 }
 BENCHMARK(BM_Bhattacharyya_Fisher);
