26e1753b54beb06a28068ee6eefbf6ff39dd83de
janbridley <bradleyjenj@gmail.com>
2025-11-25 14:29:25 -0500

    More pybind cleanup

--- src/export-module.cc
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021-2025 The Regents of the University of Michigan
-// Part of spatula, released under the BSD 3-Clause License.
-
-#include <pybind11/pybind11.h>
-
-#include "BOOSOP.h"
-#include "PGOP.h"
-#include "data/Quaternion.h"
