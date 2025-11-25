e44ed9fb6d310876e1a54201e14b18666b8afb13
janbridley <bradleyjenj@gmail.com>
2025-11-25 14:32:43 -0500

    Export threads -> header only

--- src/nanobind/export-threads.h
+++ src/nanobind/export-threads.h
@@ -2,9 +2,17 @@
 // Part of spatula, released under the BSD 3-Clause License.
 
 #pragma once
+#include "Threads.h"
 
 #include <nanobind/nanobind.h>
 
+namespace nb = nanobind;
+
 namespace spatula { namespace util {
-void export_threads(nanobind::module_& m);
-}} // namespace spatula::util
+void export_threads(nb::module_& m)
+{
+    m.def("set_num_threads",
+          [](size_t num_threads) { ThreadPool::get().set_threads(num_threads); });
+    m.def("get_num_threads", []() { return ThreadPool::get().get_num_threads(); });
+}
+}} // End namespace spatula::util
