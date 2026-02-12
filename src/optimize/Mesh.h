2be46de2d2c1993613fd7c9593c349c682865ed0
janbridley <bradleyjenj@gmail.com>
2026-02-12 09:20:48 -0500

    Use f32 rotations in optimizer

--- src/optimize/Mesh.h
+++ src/optimize/Mesh.h
@@ -59,7 +59,7 @@ class Mesh : public Optimizer {
 
     private:
     /// The set of points to evaluate.
-    std::vector<data::Vec3d> m_points;
+    std::vector<data::Vec3f> m_points;
 };
 
 }} // namespace spatula::optimize
