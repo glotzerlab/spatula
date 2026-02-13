66f458567ae7022c9f2982f22a1aa01a5cb00097
janbridley <bradleyjenj@gmail.com>
2026-02-13 13:58:24 -0500

    Minimal working changes

--- src/BOOSOP.cc
+++ src/BOOSOP.cc
@@ -177,7 +177,7 @@ void BOOSOP<distribution_type>::execute_func(std::function<void(size_t, size_t)>
         util::ThreadPool::get().serial_compute<void, size_t>(0, N, func);
     } else {
         auto& pool = util::ThreadPool::get().get_pool();
-        pool.submit_blocks(0, N, func, 2 * pool.get_thread_count());
+        pool.detach_blocks(0, N, func, 2 * pool.get_thread_count());
         pool.wait();
     }
 }
